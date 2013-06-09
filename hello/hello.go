a3c0cdc5b6812c48ccb040ff5e3895ddb28e0a33
Jędrzej Kalinowski <kalinet@poczta.onet.pl>
2013-06-09 16:42:10 +0000

    2

--- hello/hello.go
+++ hello/hello.go
@@ -1,14 +1,65 @@
 package hello
 
 import (
+    "appengine"
+    "appengine/user"
     "fmt"
     "net/http"
+    "html/template"
 )
 
 func init(){
-    http.HandleFunc("/", handler)
+    http.HandleFunc("/", root)
+    http.HandleFunc("/sign", sign)
 }
 
+func root(w http.ResponseWriter, r *http.Request){
+    return fmt.Frpint(w, guestbookForm)
+}
+
+const guestbookForm = `
+<html>
+    <body>
+        <form action="/sign" method="post">
+            <div><textarea name="content" rows="3" cols="60"></textarea></div>
+            <div><input type="submit" value="Sign guestbook"/></div>
+        </form>
+    </body>
+</html>
+`
+
+func sign(w http.ResponseWriter, r *http.Request){
+    err := signTemplate.Execute(w, r.FormValue("content"))
+    if err != nil{
+        http.Error(w,err.Error(), http.StatusInternalServerError)
+    }
+}
+
+var signTemplate = template.Must(template.New("sign").Parse(signTemplateHTML))
+
+const signTemplateHTML = `
+<html>
+    <body>
+        <p>You wrote: </p>
+        <pre>{{.}}</pre>
+    </body>
+</html>
+`
+
 func handler(w http.ResponseWriter, r *http.Request) {
+    c := appengine.NewContext(r)
+    u := user.Current(c)
+    if u == nil {
+        url, err := user.LoginURL(c, r.URL.String())
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        w.Header().Set("Location", url)
+        w.WriteHeader(http.StatusFound)
+        return
+    }
     fmt.Fprint(w, "Hello, world")
 }
+
+
